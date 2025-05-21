2a28edcd5adbd81eb3702349f3949d4df3bb26ca
ahmed <bradiiahmed@isimsf.u-sfax.tn>
2025-05-21 10:47:16 +0100

    v.0

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
 import tanStackRouterVite from "@tanstack/router-plugin/vite"; // Updated import statement
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
